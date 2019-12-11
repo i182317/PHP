0d5b98e0c89d4cdf91205b427605c662e8f878f4
-v <nathanlefort53@gmail.com>
2019-12-11 15:24:41 +0100

    fonction testLogin dans connection controller

--- Sprint_3/CodeIgniter-3.1.11/application/controllers/connection.php
+++ Sprint_3/CodeIgniter-3.1.11/application/controllers/connection.php
@@ -1,7 +1,19 @@
 <?php
 class connection extends CI_Controller {
 
-    public function verifLog(){
+    public function testLogin($username, $password){
         $this->load->model('reviews_model');
+        $connectionTest = $this->connection_model->getLog($username, $password);
+        foreach ($connectionTest as $ligne) {
+            $connectionValidate = true;
+            $idUser = $ligne['idUser'];
+        }
+
+        if($connectionTest) {
+            $this->load->view('projet_view', $idUser);
+        }
+        else{
+            $this->load->view('connection_view');
+        }
     }
 }
